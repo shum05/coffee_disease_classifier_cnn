8a5e27f3fcd18fcc8a43d807568c1391e1f16aad
shum05 <tshumetie5@gmail.com>
2023-09-04 02:47:42 -0700

    prediction code updated

--- src/coffee_cnn_Classifier/pipeline/predict.py
+++ src/coffee_cnn_Classifier/pipeline/predict.py
@@ -3,24 +3,43 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
 
+# class HemileiaVastatrix:
+#     def __init__(self,filename):
+#         self.filename =filename
+
+#     def predictioncoffeeleafrust(self):
+#         # load model
+#         model = load_model(os.path.join("artifacts","training", "model.h5"))
+
+#         imagename = self.filename
+#         test_image = image.load_img(imagename, target_size = (224,224))
+#         test_image = image.img_to_array(test_image)
+#         test_image = np.expand_dims(test_image, axis = 0)
+#         result = np.argmax(model.predict(test_image), axis=1)
+#         print(result)
+
+#         if result[0] == 1:
+#             prediction = 'healthy'
+#             return [{ "image" : prediction}]
+#         else:
+#             prediction = 'unhealthy'
+#             return [{ "image" : prediction}]
 class HemileiaVastatrix:
-    def __init__(self,filename):
-        self.filename =filename
+    def __init__(self, filename):
+        self.filename = filename
 
     def predictioncoffeeleafrust(self):
-        # load model
-        model = load_model(os.path.join("artifacts","training", "model.h5"))
+        # Load model
+        model = load_model(os.path.join("artifacts", "training", "model.h5"))
 
         imagename = self.filename
-        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.load_img(imagename, target_size=(224, 224))
         test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis = 0)
-        result = np.argmax(model.predict(test_image), axis=1)
-        print(result)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = model.predict(test_image)
 
-        if result[0] == 1:
-            prediction = 'healthy'
-            return [{ "image" : prediction}]
-        else:
-            prediction = 'unhealthy'
-            return [{ "image" : prediction}]
+        # Assuming the classes are 0 for healthy and 1 for unhealthy
+        class_labels = ["healthy", "unhealthy"]
+        predicted_class = class_labels[np.argmax(result)]
+
+        return [{"image": predicted_class, "probabilities": result.tolist()}]
